6f23c51189f9f102b09248ff61f6af9920915442
jefflicode <jefflee2230@gmail.com>
2021-10-22 16:24:43 -0700

    user profile details

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,11 +2,14 @@ import express from 'express'
 // import asyncHandler from 'express-async-handler'
 const router = express.Router()
 // import Product from '../models/productModel.js'
-import {authUser, registerUser, getUserProfile} from '../controllers/userController.js'
+import {authUser, 
+        registerUser, 
+        getUserProfile,
+        updateUserProfile,} from '../controllers/userController.js'
 import {protect} from '../middleware/authMiddleware.js'
 
 router.route('/').post(registerUser)
 router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
 
 export default router
