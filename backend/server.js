3e03f99feaa1a4c8411536ea16eac1ef1e1c4daa
jefflicode <jefflee2230@gmail.com>
2021-10-24 18:15:05 -0700

    complete project and ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 // here we are just setting up the backend server
+// using ES module need to include .js at the end of file name
 
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
-import connectDB from './config/db.js'   // using ES module need to include .js at the end of file name
-// import products from './data/products.js'
 import colors from 'colors'
+import morgan from 'morgan'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import userRoutes from './routes/userRoutes.js'
-
+import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 
@@ -17,19 +20,46 @@ connectDB()
 
 const app = express()
 
-app.use(express.json())
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
-app.get('/', (req, res) => {
-    res.send('API is running....')
-})
+app.use(express.json())
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
+
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
+
+// in this file we are using ES js, but _dirtname requires common js
+// using path.resolve() to handle this syntax issue
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
 app.use(notFound)
-
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+)
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,57 +1,70 @@
 import React, { useEffect } from 'react'
 // using useEffect to make request to the backend
+import { Link } from 'react-router-dom'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { useDispatch, useSelector } from 'react-redux'
 import { listProducts } from '../actions/productActions'
+import Paginate from '../components/Paginate'
+import ProductCarousel from '../components/ProductCarousel'
+import Meta from '../components/Meta'
 // import axios from 'axios'
 
 // originally storing json file in frontend, removed since we want to fetch data from backend
 // import products from '../products'
 
-const HomeScreen = () => {
-    // const [products, setProducts] = useState([])
+const HomeScreen = ({match}) => {
+    const keyword = match.params.keyword
+
+    const pageNumber = match.params.pageNumber || 1
+
     const dispatch = useDispatch()
 
-    const productList = useSelector(state => state.productList)
-    const { loading, error, products } = productList
+    const productList = useSelector((state) => state.productList)
+    const { loading, error, products, page, pages } = productList
 
     // as soon as the screen loads, whatever is inside is useEffect will get fired off right away
     // that's when we should make request to the backend and get the products data
     // if you would put console.log('hello') in it, you will it hello gets printed on the console
     // as soon as the web page (the component) loads
     useEffect(() => {
-        // console.log('hello')
-        // const fetchProducts = async () => {
-        //     /* We set up a proxy in the json file to connect to back end and avoid cors error */
-        //     const { data } = await axios.get('/api/products') 
-
-        //     setProducts(data)
-        // }
-
-        // fetchProducts()
-        dispatch(listProducts())
-    }, [dispatch] )
+        dispatch(listProducts(keyword, pageNumber))
+    }, [dispatch, keyword, pageNumber] )
 
     return (
         <>
-            <h1>Latest Products</h1>
-            {loading ? (
-                <Loader />
-            )   : error ? (
-                <Message variant='danger'>{error}</Message>
-            )
-                : <Row>
-                    {products.map(product => (
-                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                            <Product product={product} />
-                        </Col>
-                    ))}
-                </Row>
-            }
+      <Meta />
+      {!keyword ? (
+        <ProductCarousel />
+      ) : (
+        <Link to='/' className='btn btn-light'>
+          Go Back
+        </Link>
+      )}
+      <h1>Latest Products</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+          <Paginate
+            pages={pages}
+            page={page}
+            keyword={keyword ? keyword : ''}
+          />
         </>
+      )}
+    </>
     )
 }
 
